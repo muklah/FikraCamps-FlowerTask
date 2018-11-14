d7cfb8477a8aa90c7fd807ab87b126c7a24ca7ad
Muklah Hilal <M-M.mexat@hotmail.com>
2018-11-14 11:10:53 +0300

    add messages to the console log

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ function filterLetter(data, index, letter) {
   return result;
 }
 
-console.log(data.length);
-console.log(filterLetter(data, 0, 'S'));
-console.log(data.length-filterLetter(data, 0, 'S').length);
-console.log(filterLetter(data, 0, 'M'));
+console.log('Number of rows in flower file', data.length);
+console.log('Flowers list start with S', filterLetter(data, 0, 'S'));
+console.log('Number of flowers which not start with S', data.length-filterLetter(data, 0, 'S').length);
+console.log(filterLetter('Flowers start with M', data, 0, 'M'));
 
 for (i=0; i < data.length; ++i) 
      if (data[i].length == 5)
-console.log(data[i]);
+console.log('List of flowers where length is 5', data[i]);
 
